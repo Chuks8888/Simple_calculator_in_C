22907141cd852ec9519e7e16849986660769aac5
Chuks8888 <hugotarczynski@gmail.com>
2023-12-14 15:54:43 +0100

    added function to realloc memory with old contents

--- prototype.c
+++ prototype.c
@@ -4,9 +4,23 @@
 
 #define Length 32
 
+void reallocate(int* previous)
+{
+	int size = sizeof(previous);
+	int *temp = malloc(size);
+
+	memcpy(temp, previous, size);
+
+	free(previous);
+	previous = malloc (size*2);
+
+	memcpy(previous, temp, size);
+}
+
 int main()
 {
     char* operation;
     operation = malloc(Length);
-    operation = fgets(operation, Length, stdin);    
+    operation = fgets(operation, Length, stdin);
+	
 }
